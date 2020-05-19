9df4e473de2b3f9258d00893fc4cd44001f09553
Davidodari <davidkibzodari@gmail.com>
2020-05-19 19:57:58 +0300

    Initial commit

---
+++ app/src/main/java/com/github/odaridavid/talkself/Constants.kt
@@ -0,0 +1,27 @@
+/**
+ *
+ * Copyright 2020 David Odari
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *            http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ **/
+package com.github.odaridavid.talkself
+
+
+const val TALK_SELF_APP_SP = "talk_self_pref"
+
+const val USER_ONE_ID_PREF_KEY = "user_one_id"
+const val USER_TWO_ID_PREF_KEY = "user_two_id"
+const val USER_ONE_NAME_PREF_KEY = "user_one_name"
+const val USER_TWO_NAME_PREF_KEY = "user_two_name"
+const val INVALID_DEFAULT_ID = -1
+const val DEFAULT_USERNAME = "Anonymous"
+
+const val USER_ONE_ID = 1
+const val USER_TWO_ID = 2
---
+++ app/src/main/java/com/github/odaridavid/talkself/TalkSelfApp.kt
@@ -0,0 +1,39 @@
+/**
+ *
+ * Copyright 2020 David Odari
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *            http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ **/
+package com.github.odaridavid.talkself
+
+import android.app.Application
+import android.content.Context
+import android.content.SharedPreferences
+
+
+class TalkSelfApp : Application() {
+
+    lateinit var sharedPref: SharedPreferences
+
+    override fun onCreate() {
+        super.onCreate()
+
+        sharedPref = getSharedPreferences(TALK_SELF_APP_SP, Context.MODE_PRIVATE)
+
+        if (sharedPref.getInt(USER_ONE_ID_PREF_KEY, INVALID_DEFAULT_ID) == INVALID_DEFAULT_ID) {
+            val editor = sharedPref.edit()
+            with(editor) {
+                putInt(USER_ONE_ID_PREF_KEY, USER_ONE_ID)
+                putInt(USER_TWO_ID_PREF_KEY, USER_TWO_ID)
+                apply()
+            }
+        }
+    }
+}
